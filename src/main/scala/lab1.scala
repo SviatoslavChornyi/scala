fcec405549ce026e0ea484a6f5bab6678d7bab6f
SviatoslavChornyi <55602382+SviatoslavChornyi@users.noreply.github.com>
2022-03-30 11:02:26 +0300

    lab
    lab1

---
+++ src/main/scala/lab1.scala
@@ -0,0 +1,60 @@
+
+import scala.io.StdIn.readLine
+//щоб скоротити код
+  object lab1 {
+
+    def main(args: Array[String]): Unit = {
+      val a = BuildMatrix(2, 2)
+      val b = BuildMatrix(2, 2)
+
+      println(Addition(a, b))
+      println(Multiplication(a, 2))
+      println(Division(a, 2.5))
+      println(Transposition(a))
+
+      DisplayMatrix(a)
+    }
+
+    def BuildMatrix(rows: Int, column: Int): List[List[Double]] = {
+      List.fill(column)(readLine().split(' ').map(_.toDouble).toList.take(rows))//мап щоб перевести в дабл
+    }
+
+    def Addition(a: List[List[Double]], b: List[List[Double]]): List[List[Double]] = {
+      var result = List[List[Double]]()
+      for (i <- 0 to 1) {
+        result = result :+
+          a(i).zip(b(i)).map {
+            case (a, b) => a + b
+          }
+      }
+      result
+    }
+
+    def Subtraction(a: List[List[Double]], b: List[List[Double]]): List[List[Double]] = {
+      var result = List[List[Double]]()
+      for (i <- 0 to 1) {
+        result = result :+
+          a(i).zip(b(i)).map {
+            case (a, b) => a - b
+          }
+      }
+      result
+    }
+
+    def Multiplication(a: List[List[Double]], b: Double): List[List[Double]] = {
+      a.map(_.map(x => x * b))
+    }
+
+    def Division(a: List[List[Double]], b: Double): List[List[Double]] = {
+      a.map(_.map(x => x / b))
+    }
+
+    def Transposition(a: List[List[Double]]): List[List[Double]] = {
+      a.transpose
+    }
+
+    def DisplayMatrix(a: List[List[Double]]): Unit = {
+      println(a.map(_.mkString(" ")).mkString("\n"))
+    }
+  }
+
